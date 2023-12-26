920f3c56ed4801dd106db7a3a5aa19b38977d53f
titovtima <titov.tima.spb@yandex.ru>
2023-12-26 18:12:14 +0300

    build fat jar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
+ktor {
+    fatJar {
+        archiveFileName.set("SongsServer.jar")
+    }
+}
+
 repositories {
     mavenCentral()
 }
