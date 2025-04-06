fb4c4492b4bc7e35bd2a4f03d5238109ea5cfea7
titovtima <titov.tima.spb@yandex.ru>
2025-04-06 20:34:29 +0300

    one working test

--- src/test/kotlin/ru/titovtima/songsserver/ApplicationTest.kt
+++ src/test/kotlin/ru/titovtima/songsserver/ApplicationTest.kt
@@ -4,19 +4,26 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.json.Json
+import ru.titovtima.songsserver.model.User
 import kotlin.test.*
-import ru.titovtima.plugins.*
 import ru.titovtima.songsserver.plugins.configureRouting
+import ru.titovtima.songsserver.plugins.configureSecurity
+import ru.titovtima.songsserver.plugins.configureSerialization
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
+            configureSecurity()
+            configureSerialization()
             configureRouting()
         }
-        client.get("/").apply {
+        client.get("/api/v1/user/titovtima").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            val user = Json.decodeFromString<User>(bodyAsText())
+            assertEquals("titovtima", user.username)
+            assertEquals(1, user.id)
         }
     }
 }
